749db7dca7c01b2e2b0aa905563cea507d9117cd
Yaroslav Rizun <Yaroslav_Rizun@epam.com>
2025-02-24 21:05:06 +0200

    Hybris MP-Facade Layer, task - 8

--- hybris/bin/custom/training/trainingfacades/src/org/training/facades/populators/CwPhysicalDimensionsProductPopulator.java
+++ hybris/bin/custom/training/trainingfacades/src/org/training/facades/populators/CwPhysicalDimensionsProductPopulator.java
@@ -4,14 +4,41 @@ import de.hybris.platform.commercefacades.product.data.ProductData;
 import de.hybris.platform.converters.Populator;
 import de.hybris.platform.core.model.product.ProductModel;
 import de.hybris.platform.servicelayer.dto.converter.ConversionException;
+import de.hybris.platform.servicelayer.session.SessionService;
+import org.training.core.enums.UnitSystemType;
+
+import javax.annotation.Resource;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class CwPhysicalDimensionsProductPopulator implements Populator<ProductModel, ProductData> {
 
+    private static final double MG_TO_GRAINS = 0.0154324;
+    private static final double CM_TO_INCHES = 0.393701;
+    private static final String UNIT_SYSTEM = "unitSystem";
+
+    @Resource
+    private SessionService sessionService;
+
     @Override
     public void populate(ProductModel productModel, ProductData productData) throws ConversionException {
-        productData.setWeight(productModel.getWeight());
-        productData.setHeight(productModel.getHeight());
-        productData.setLength(productModel.getLength());
-        productData.setWidth(productModel.getWidth());
+        boolean useImperial = isImperialSystem();
+
+        productData.setWeight(convertValue(productModel.getWeight(), useImperial ? MG_TO_GRAINS : 1.0));
+        productData.setHeight(convertValue(productModel.getHeight(), useImperial ? CM_TO_INCHES : 1.0));
+        productData.setLength(convertValue(productModel.getLength(), useImperial ? CM_TO_INCHES : 1.0));
+        productData.setWidth(convertValue(productModel.getWidth(), useImperial ? CM_TO_INCHES : 1.0));
+    }
+
+    private boolean isImperialSystem() {
+        UnitSystemType unitSystem = sessionService.getAttribute(UNIT_SYSTEM);
+        return UnitSystemType.IMPERIAL.equals(unitSystem);
+    }
+
+    private double convertValue(Double value, double conversionFactor) {
+        if (value == null) {
+            return 0.0;
+        }
+        return Math.ceil(value * conversionFactor * 100) / 100.0;
     }
 }
--- hybris/bin/custom/training/trainingfacades/testsrc/org/training/facades/suggestion/CwDefaultProductFacadeIntegrationTest.java
+++ hybris/bin/custom/training/trainingfacades/testsrc/org/training/facades/suggestion/CwDefaultProductFacadeIntegrationTest.java
@@ -4,8 +4,10 @@ import de.hybris.bootstrap.annotations.IntegrationTest;
 import de.hybris.platform.commercefacades.product.ProductOption;
 import de.hybris.platform.commercefacades.product.data.ProductData;
 import de.hybris.platform.servicelayer.ServicelayerTransactionalTest;
+import de.hybris.platform.servicelayer.session.SessionService;
 import org.junit.Before;
 import org.junit.Test;
+import org.training.core.enums.UnitSystemType;
 import org.training.facades.product.CwDefaultProductFacade;
 
 import javax.annotation.Resource;
@@ -18,13 +20,17 @@ import static org.junit.Assert.assertNull;
 public class CwDefaultProductFacadeIntegrationTest extends ServicelayerTransactionalTest {
 
     private static final String ProductCode = "product1";
+    private static final String UNIT_SYSTEM = "unitSystem";
 
     @Resource
     private CwDefaultProductFacade cwDefaultProductFacade;
+    @Resource
+    private SessionService sessionService;
 
     @Before
     public void setUp() throws Exception {
         importCsv("/trainingfacades/test/testProduct.impex", "utf-8");
+        sessionService.setAttribute(UNIT_SYSTEM, UnitSystemType.METRIC);
     }
 
     @Test
@@ -48,5 +54,19 @@ public class CwDefaultProductFacadeIntegrationTest extends ServicelayerTransacti
         assertNull(productData.getHeight());
         assertNull(productData.getLength());
         assertNull(productData.getWidth());
+    }
+
+    @Test
+    public void shouldRetrieveProductWithConvertedPhysicalDimensionsImperial() {
+        sessionService.setAttribute(UNIT_SYSTEM,UnitSystemType.IMPERIAL);
+
+        ProductData productData = cwDefaultProductFacade.getProductForCodeAndOptions(ProductCode, Collections.singleton(ProductOption.PHYSICAL_DIMENSIONS));
+
+        assertEquals(ProductCode, productData.getCode());
+
+        assertEquals(23.15, productData.getWeight(), 0.01); // 1500 mg equals 23.15 grains
+        assertEquals(4.13, productData.getWidth(), 0.01);   // 10.5 cm equals 4.13 inches
+        assertEquals(7.87, productData.getLength(), 0.01);  // 20.0 cm equals 7.87 inches
+        assertEquals(2.17, productData.getHeight(), 0.01);  // 5.5 cm equals 2.17 inches
     }
 }
